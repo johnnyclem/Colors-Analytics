162f1a79725262a3cb079bcd1e67db7001b1c4a6
John Clem <johnnyclem@gmail.com>
2014-11-19 09:32:41 -0800

    Initial Commit

---
+++ Colors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colors
+//
+//  Created by John Clem on 11/19/14.
+//  Copyright (c) 2014 Analytics Pros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Colors
+//
+//  Created by John Clem on 11/19/14.
+//  Copyright (c) 2014 Analytics Pros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
